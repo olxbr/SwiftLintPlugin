8e19f8111bd623d99aab2754cf4e03efbc402be7
Gabriel Reynoso <gbr.reynoso@icloud.com>
2022-09-16 11:36:16 -0300

    Create SwiftLint and SwiftLintCommand plugins

--- Package.swift
+++ Package.swift
@@ -6,8 +6,29 @@ import PackageDescription
 let package = Package(
     name: "SwiftLintPlugin",
     products: [
+        .plugin(name: "SwiftLint", targets: ["SwiftLint"]),
+        .plugin(name: "SwiftLintCommand", targets: ["SwiftLintCommand"])
     ],
-    dependencies: [],
     targets: [
+        .plugin(
+            name: "SwiftLint",
+            capability: .buildTool(),
+            dependencies: ["SwiftLintBinary"]
+        ),
+        .plugin(
+            name: "SwiftLintCommand",
+            capability: .command(
+                intent: .custom(
+                    verb: "swiftlint",
+                    description: "Execute SwiftLint."
+                )
+            ),
+            dependencies: ["SwiftLintBinary"]
+        ),
+        .binaryTarget(
+            name: "SwiftLintBinary",
+            url: "https://github.com/realm/SwiftLint/releases/download/0.48.0/SwiftLintBinary-macos.artifactbundle.zip",
+            checksum: "9c255e797260054296f9e4e4cd7e1339a15093d75f7c4227b9568d63edddba50"
+        )
     ]
 )
---
+++ Plugins/SwiftLint/main.swift
@@ -0,0 +1,38 @@
+import Foundation
+import PackagePlugin
+
+@main
+struct SwiftLintPlugin: BuildToolPlugin {
+
+    func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
+        let arguments = arguments(
+            packageDirectory: context.package.directory.string,
+            targetDirectory: target.directory.string
+        )
+        return [
+            .buildCommand(
+                displayName: "Running SwiftLint for \(target.name)",
+                executable: try context.tool(named: "swiftlint").path,
+                arguments: arguments,
+                environment: [:]
+            )
+        ]
+    }
+
+    func arguments(packageDirectory: String, targetDirectory: String) -> [String] {
+        var arguments = [
+            "lint",
+            "--in-process-sourcekit"
+        ]
+
+        let lintConfigFilePath = "\(packageDirectory)/.swiftlint.yml"
+        if FileManager.default.fileExists(atPath: lintConfigFilePath) {
+            arguments.append(
+                contentsOf: ["--config", lintConfigFilePath]
+            )
+        }
+
+        arguments.append(targetDirectory)
+        return arguments
+    }
+}
